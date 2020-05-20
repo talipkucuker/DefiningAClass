e51c0c69fe6428fedb791120efc2608cf902a424
talipkucuker <talipkucuker@talips-mbp.attlocal.net>
2020-05-20 02:45:47 -0400

    Logical Op Table

---
+++ DefiningAClass/src/LogicalOpTable/LogicalOpTable.java
@@ -0,0 +1,33 @@
+package LogicalOpTable;
+
+public class LogicalOpTable {
+	public static void main(String[] args) {
+		
+		boolean p, q;
+		
+		System.out.println("P\tQ\tAnd\tOr\tXor\tNot");
+		
+		p = true; q = true;
+		System.out.print(p + "\t" + q + "\t");
+		System.out.print((p&q) + "\t" + (p|q) + "\t");
+		System.out.println((p^q) + "\t" + (!p));
+		
+		p = true; q = false;
+		System.out.print(p + "\t" + q + "\t");
+		System.out.print((p&q) + "\t" + (p|q) + "\t");
+		System.out.println((p^q) + "\t" + (!p));
+		
+		
+		p = false; q = true;
+		System.out.print(p + "\t" + q + "\t");
+		System.out.print((p&q) + "\t" + (p|q) + "\t");
+		System.out.println((p^q) + "\t" + (!p));
+		
+		p = false; q = false;
+		System.out.print(p + "\t" + q + "\t");
+		System.out.print((p&q) + "\t" + (p|q) + "\t");
+		System.out.println((p^q) + "\t" + (!p));
+		
+		
+	}
+}
