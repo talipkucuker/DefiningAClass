b3b1d099e616d792d098ebf3f4d32614d316c57f
talipkucuker <talipkucuker@talips-mbp.attlocal.net>
2020-05-19 23:51:56 -0400

    Defining a class First Commit + add method

---
+++ DefiningAClass/src/DefiningAClass/Vehicle.java
@@ -0,0 +1,13 @@
+package DefiningAClass;
+
+public class Vehicle {
+
+	int passengers; //number of passengers
+	int fuelcap; //fuel capacity in gallons
+	int mpg; //fuel consumption in miles per gallon
+	
+	void range() {
+		System.out.println("Range is " + fuelcap * mpg);
+	}
+}
+
